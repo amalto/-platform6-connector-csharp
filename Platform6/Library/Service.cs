e5d4f66f23112cff4c154989d256d92b9c4ae867
Roxane Macé <roxane.mace@isep.fr>
2018-04-05 16:21:25 +0200

    Do not send a common message with an attachment null

--- Platform6/Library/Service.cs
+++ Platform6/Library/Service.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using static Com.Amalto.Imdg.Cm.CommonMessage.Types;
+
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Com.Amalto.Imdg.Cm;
@@ -7,6 +9,7 @@ using Hazelcast.Config;
 using Hazelcast.Core;
 using Library.Models;
 using Library.Serializers;
+using Newtonsoft.Json;
 
 namespace Library {
     public class Service {
@@ -21,7 +24,6 @@ namespace Library {
         }
 
         private async Task CreateHazelcastClient() {
-            // Start the Hazelcast C# Client and connect to the b2box Halzelcast
             Environment.SetEnvironmentVariable("hazelcast.logging.level", "info");
             Environment.SetEnvironmentVariable("hazelcast.logging.type", "console");
 
@@ -35,7 +37,7 @@ namespace Library {
             config.GetSerializationConfig().AddSerializerConfig(serializerConfig);
             config.GetNetworkConfig().AddAddress(hostname + ":" + port);
 
-            Client = await Task.Run(() => HazelcastClient.NewHazelcastClient(config));
+            Client = Task.Run(() => HazelcastClient.NewHazelcastClient(config)).Result;
         }
 
         private async Task DeployService(DeployParameters parameters) {
@@ -45,38 +47,40 @@ namespace Library {
                 Username = parameters.Username,
                 ReceiverId = Constants.ServiceManagerId,
                 Action = Constants.ActionDeploy,
-                Headers = new List<CommonMessage.Types.Header> {
+                Headers = new List<Header> {
                     BusConnection.CreateHeader(Constants.ServiceManagerId, "node.id", Guid.NewGuid().ToString()),
                     BusConnection.CreateHeader(Constants.ServiceManagerId, "service.id", parameters.Id),
                     BusConnection.CreateHeader(Constants.ServiceManagerId, "service.path", parameters.Path),
                     BusConnection.CreateHeader(Constants.ServiceManagerId, "service.ctx", parameters.BasePath),
                     BusConnection.CreateHeader(Constants.ServiceManagerId, "service.version", parameters.Versions.Server),
                     BusConnection.CreateHeader(Constants.ServiceManagerId, "service.ui.version", parameters.Versions.Client),
-                    BusConnection.CreateHeader(Constants.ServiceManagerId, "service.ui", parameters.Ui)
+                    BusConnection.CreateHeader(Constants.ServiceManagerId, "service.ui", JsonConvert.SerializeObject(parameters.Ui))
                 }
             });
         }
 
         public async Task<CommonMessage> CallService(CallServiceParameters parameters) {
             var receiverId = parameters.ReceiverId;
-            var headers = new List<CommonMessage.Types.Header> {BusConnection.CreateHeader(null, Constants.UserKey, parameters.Username)};
+            var headers = new List<Header> {BusConnection.CreateHeader(null, Constants.UserKey, parameters.Username)};
+            var attachments = parameters.Attachments ?? new List<Attachment>();
 
             if (parameters.Action != null) headers.Add(BusConnection.CreateHeader(receiverId, Constants.Action, parameters.Action));
             if (parameters.Headers != null) headers.AddRange(parameters.Headers);
 
-            return await SendCommonMessage(receiverId, await Task.Run(() => BusConnection.CreateCommonMessage(_idKey, parameters.ReceiverId, headers, parameters.Attachments)));
+            var commonMessage = Task.Run(() => BusConnection.CreateCommonMessage(_idKey, parameters.ReceiverId, headers, attachments)).Result;
+            return await SendCommonMessage(receiverId, commonMessage);
         }
 
         public async Task<CommonMessage> SendCommonMessage(string receiverId, CommonMessage commonMessage) {
             var receiverIdKey = Constants.ReceiverIdPrefix + receiverId;
             var request = Client.GetQueue<CommonMessage>(receiverIdKey);
-            var isSent = await Task.Run(() => request.Offer(commonMessage));
+            var isSent = Task.Run(() => request.Offer(commonMessage)).Result;
 
             if (!isSent) throw new Exception("Unable to send the common message with id " + commonMessage.Id + "!");
 
             BusConnection.DisplayCommonMessage(receiverIdKey, commonMessage);
 
-            var response = await Task.Run(() => Client.GetQueue<CommonMessage>(_idKey).Take());
+            var response = Task.Run(() => Client.GetQueue<CommonMessage>(_idKey).Take()).Result;
 
             BusConnection.DisplayCommonMessage(receiverIdKey, response);
 
