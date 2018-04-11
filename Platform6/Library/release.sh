1d1b55ce38fe3f4d909e41d0af10406f2566e143
Roxane Macé <roxane.mace@isep.fr>
2018-04-11 10:36:32 +0200

    Update the release script (#8)

--- Platform6/Library/release.sh
+++ Platform6/Library/release.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+# Check for a valid version
+if [[ $1 == "" ]]; then
+	echo 'You must provide a valid version as a first argument.'
+	exit 1
+fi
+
 # Check branch is master
 if [ $(git symbolic-ref --short HEAD) != 'master' ]; then
 	echo 'You must be on branch master to release a new version.'
@@ -15,9 +21,16 @@ fi
 # Check branch is up-to-date
 git fetch
 
-# Build the project
-nuget pack
+# Update .nuspec file
+sed -i '' -e "s/<version>.*<\/version>/<version>$1<\/version>/g" ./.nuspec
 
-# Commit and tag the release
-git commit -am "Bump to version ${TAG:1}"
-git tag "$1"
+# Build the project
+msbuild Library.csproj
+
+# Create the NuGet package
+cd releases/
+nuget pack ../Library.csproj
+
+# Tag the release
+git commit -am "Bump to version $1"
+git tag "v$1"
